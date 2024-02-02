57cc669cfa4f29ab963a3c728748c20ad40bd3bd
jszady <jszady99@gmail.com>
2024-02-01 23:34:38 -0500

    fixed app.jsx

--- frontend/src/pages/property.jsx
+++ frontend/src/pages/property.jsx
@@ -1,33 +1,38 @@
 import PropertyDetials from "./components/PropertyDetails/PropertyDetails";
-import LeaseDetails from "./components/PropertyDetails/LeaseDetails";
+import LeaseDetails from "./components/PropertyDetails/leaseDetails";
 import ProfitLoss from "./components/PropertyDetails/ProfitLoss";
-import './components/PropertyDetails/PropertyDetails.css'
+import "./components/PropertyDetails/PropertyDetails.css";
 import { useState, useEffect } from "react";
-import { Routes, Route, useParams } from 'react-router-dom';
+import { Routes, Route, useParams } from "react-router-dom";
 
 function Property() {
-
   const [property, setProperty] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
     fetch(`http://localhost:3001/api/property/details/${id}`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         //console.log('Fetched property data:', data);
         setProperty(data);
       })
-      .catch(error => console.error('Error fetching property data', error));
+      .catch((error) => console.error("Error fetching property data", error));
   }, []);
 
   const prop = property && property[0];
 
   return (
-    <div >
+    <div>
       <h1>{prop?.address}</h1>
-      <div className="property-details-cards"><PropertyDetials property={property} /> </div>
-      <div className="property-details-cards"><LeaseDetails property={property}/> </div>
-      <div className="property-details-cards"><ProfitLoss property={property}/> </div>
+      <div className="property-details-cards">
+        <PropertyDetials property={property} />{" "}
+      </div>
+      <div className="property-details-cards">
+        <LeaseDetails property={property} />{" "}
+      </div>
+      <div className="property-details-cards">
+        <ProfitLoss property={property} />{" "}
+      </div>
     </div>
   );
 }
