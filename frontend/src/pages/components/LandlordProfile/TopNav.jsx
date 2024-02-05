592f624fda1ea26063d42900ebba29deaf3ca89e
Jill Regan <jillian.jnr@hotmail.com>
2024-02-05 14:37:33 -0500

    Fix: add property button on top nav now links to /new page

--- frontend/src/pages/components/LandlordProfile/TopNav.jsx
+++ frontend/src/pages/components/LandlordProfile/TopNav.jsx
@@ -1,6 +1,7 @@
 import "./topnav.css";
 import landlord from "./data/landlord";
 import image from "./data/photos/profilePic.png"
+import { Link } from 'react-router-dom';
 
 export default function TopNav({name}) {
 
@@ -10,7 +11,9 @@ export default function TopNav({name}) {
         <img src={image} alt="profilepic" class="blue-box" />
           <div class="user-welcome">
             <h3>Welcome {name}</h3>
+            <Link to={`/new`}>
             <button className="add-p-btn">Add Property</button>
+            </Link>
           </div>
     </div>
   );
