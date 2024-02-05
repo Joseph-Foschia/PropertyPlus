10885bb5e5eefb1ee052d8adbbea82530f2c3ba6
jszady <jszady99@gmail.com>
2024-02-05 11:39:56 -0500

    working on prop detail

--- frontend/src/pages/components/LandlordProfile/PropertyListItem.jsx
+++ frontend/src/pages/components/LandlordProfile/PropertyListItem.jsx
@@ -1,28 +1,24 @@
 import "./propertyListItem.css";
 import propertyList from "./data/propertyLists";
 
-export default function PropertyListItem({userData, address, status}) {
+export default function PropertyListItem({ userData, address, status }) {
   return (
     <div>
-      
-        <div className="propertyListItem" >
-          
-          <img className="profile-pic" alt="property" />
-          <div className="name-status">
-            <div className="p-name">{address}</div>
-            <div
-              className={`status ${
-                status === "Occupied" ? "occupied" : "available"
-              }`}
-            >
-              {status === "Occupied" ? "Occupied" : "Available"}
-            </div>
+      <div className="propertyListItem">
+        <img className="profile-pic" alt="property" />
+        <div className="name-status">
+          <div className="p-name">{address}</div>
+          <div
+            className={`status ${
+              status === "Occupied" ? "occupied" : "available"
+            }`}
+          >
+            {status === "Occupied" ? "Occupied" : "Available"}
           </div>
         </div>
-      
+      </div>
     </div>
-
-        // <div>
+    // <div>
     //   {propertyList.map((property, index) => (
     //     <div className="propertyListItem" key={index}>
     //       {" "}
---
+++ frontend/src/pages/components/PropertyDetails/PropertyTopNav.jsx
@@ -0,0 +1,14 @@
+import "./propertyTopNav.css";
+export default function PropertyTopNav() {
+  return (
+    <div className="top-p-nav">
+      <div className="nav-container">
+        <div className="text-nav">
+          <h1 className="welcome">Welcome [User]</h1>
+          <p className="p-pro">Welcome to ProperyPro</p>
+        </div>
+        <div className="profile-pic"></div>
+      </div>
+    </div>
+  );
+}
