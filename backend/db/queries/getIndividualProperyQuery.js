4b11f580bd9e49dd4cea334343c6f322e2638a48
Jill Regan <jillian.jnr@hotmail.com>
2024-02-06 14:23:11 -0500

    Fix: Details pull from database

--- backend/db/queries/getIndividualProperyQuery.js
+++ backend/db/queries/getIndividualProperyQuery.js
@@ -3,10 +3,11 @@ const db = require('../connection');
 const getIndividualProperty =  (id) => {
  
   return db
-  .query(`SELECT units.*, leases.*, tenants.* 
+  .query(`SELECT units.*, leases.*, tenants.name as tenant_name, tenants.phone_number as tenant_phone, landlords.* 
   FROM units 
   JOIN leases ON units.id = leases.unit_id 
   JOIN tenants ON leases.tenant_id = tenants.id 
+  JOIN landlords ON units.landlord_id = landlords.id
   WHERE units.id = $1`, [id])
   .then((res) => {
     return res.rows;
