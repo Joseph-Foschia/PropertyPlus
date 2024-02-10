2912178c80e9fe36b6e10dadb47ff3265fb94bb5
jszady <jszady99@gmail.com>
2024-02-10 12:39:29 -0500

    nav style done

--- frontend/src/pages/TenantDashboard.jsx
+++ frontend/src/pages/TenantDashboard.jsx
@@ -6,26 +6,25 @@ import LeaseDetails from "./components/tenant-dash/LeaseDetails";
 import SomeBox from "./components/tenant-dash/SomeBox";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { fetchTenantMaintenance, fetchTenantLease } from "../helpers/tenantDashHelper";
+import {
+  fetchTenantMaintenance,
+  fetchTenantLease,
+} from "../helpers/tenantDashHelper";
 import { handleEmailLandlord } from "../helpers/emailHelper";
 
-
-
 export default function TenantDashboard({ token, userData }) {
-  
   //Details for tenants lease
   const [lease, setLease] = useState([]);
-  const [maintenance, setMaintenance] = useState([])
+  const [maintenance, setMaintenance] = useState([]);
 
   //Makes api call for logged in tenants lease and also their maintenance requests
   useEffect(() => {
-    fetchTenantLease(userData, setLease)
-    fetchTenantMaintenance(userData, setMaintenance)
+    fetchTenantLease(userData, setLease);
+    fetchTenantMaintenance(userData, setMaintenance);
   }, []);
 
   //ensures lease is true and sets lease detaisl to the first value of lease
   const tenantDetails = lease && lease[0];
-
 
   return (
     <div>
@@ -38,14 +37,19 @@ export default function TenantDashboard({ token, userData }) {
         <PaymentDetails />
       </div>
       <div className="bottom-ten-dash">
-        <LeaseDetails tenantDetails={tenantDetails}/>
-        <SomeBox tenantDetails={tenantDetails} maintenance={maintenance}/>
+        <LeaseDetails tenantDetails={tenantDetails} />
+        <SomeBox tenantDetails={tenantDetails} maintenance={maintenance} />
         <div className="t-buttons">
           <button className="t-m-req">Maintenace Request</button>
           <Link to="/pay">
             <button className="t-p-rent">Pay Rent</button>
           </Link>
-          <button className="t-m-req" onClick={() => handleEmailLandlord(tenantDetails)}>Email Landlord</button>
+          <button
+            className="t-m-req"
+            onClick={() => handleEmailLandlord(tenantDetails)}
+          >
+            Email Landlord
+          </button>
         </div>
       </div>
     </div>
