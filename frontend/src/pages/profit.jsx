f605b9bfefdc85ad93cbd44b9b465ddd8678ca01
YoasterToaster <joefoschia@gmail.com>
2024-02-23 12:16:53 -0500

    Fixed an issue with Profit.jsx where old code was still there

--- frontend/src/pages/profit.jsx
+++ frontend/src/pages/profit.jsx
@@ -26,60 +26,7 @@ function Profits(props) {
     setMaintenanceData,
     setTotalMaintenanceCost
   );
-
-  // Average Rent
-  useEffect(() => {
-    fetch("http://localhost:3001/api/margins/rent/1")
-      .then((response) => response.json())
-      .then((data) => {
-        setAverageRent(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching average rent data:", error);
-      });
-  }, []);
-
-  // Occupancy Rate
-  useEffect(() => {
-    fetch("http://localhost:3001/api/margins/occupancy/1")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("We are in profit.jsx: ", data);
-        setOccupancyRate(data.occupancy_rate);
-      })
-      .catch((error) => {
-        console.error("Error fetching maintenance data:", error);
-      });
-  }, []);
-
-  // Total Maintenance Costs
-  useEffect(() => {
-    fetch("http://localhost:3001/api/margins/maintenancetotal/1")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(
-          "We are in profit.jsx Total Maintenance Costs: ",
-          data.total_maintenance_cost
-        );
-        setTotalMaintenanceCost(data.total_maintenance_cost);
-      })
-      .catch((error) => {
-        console.error("Error fetching maintenance data:", error);
-      });
-  }, []);
-
-  // Maintenance Data
-  useEffect(() => {
-    fetch("http://localhost:3001/api/margins/maintenance/1")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("MAINTENANCE DATA: ", data);
-        setMaintenanceData(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching maintenance data:", error);
-      });
-  }, []);
+  
   return (
     <div>
       <Nav />
